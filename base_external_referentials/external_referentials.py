f47f11280e7d67a536b4cbe8735727318f3ec18e
manu <esamyn@gmail.com>
2012-02-09 12:46:29 +0100

    [UPDATE] create_mapping_file id auto generation method

--- base_external_referentials/external_referentials.py
+++ base_external_referentials/external_referentials.py
@@ -324,11 +324,13 @@ class external_mapping(osv.osv):
         mapping = self.browse(cr, uid, ids)[0]
     	for line in mapping.mapping_ids:
     		if line.external_field!=False and line.active==True:
-    			mapping_line += "\""+mapping.referential_id.version_id.name+"_erp_"+line.external_field+"\","
+    		    current_model = mapping.model_id.get_external_id(context=context)[mapping.model_id.id]
+    		    current_field = line.field_id.get_external_id(context=context)[line.field_id.id]
+    			mapping_line += "\""+mapping.referential_id.version_id.name+"_"+current_model+"_"+current_field+"_"+line.external_field+"\","
     			mapping_line += "\""+mapping.referential_id.version_id.name+"\","
-    			mapping_line += "\""+mapping.model_id.get_external_id(context=context)[mapping.model_id.id]+"\","
+    			mapping_line += "\""+current_model+"\","
     			mapping_line += "\""+line.external_field+"\","
-    			mapping_line += "\""+line.field_id.get_external_id(context=context)[line.field_id.id]+"\","
+    			mapping_line += "\""+current_field+"\","
     			mapping_line += "\""+line.type+"\","
     			if line.evaluation_type!=False:
     				mapping_line += "\""+line.evaluation_type+"\""

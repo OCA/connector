3a30f51406950a87d4f907ffa9ed4c822b93e344
sebastien beau <sebastien.beau@akretion.com.br>
2012-02-23 21:55:48 +0100

    [IMP] base_file_protocole : add 'filestore' protocole

--- base_file_protocole/base_file_protocole.py
+++ base_file_protocole/base_file_protocole.py
@@ -24,35 +24,51 @@ import netsvc
 from tempfile import TemporaryFile
 from ftplib import FTP
 import sys
+import os
+import shutil
 
 class FileConnection(object):
 
+    def is_(self, protocole):
+        return self.protocole.lower() == protocole
+
     def __init__(self, protocole, location, user, pwd):
         self.protocole = protocole
-        if protocole.lower() == 'ftp':
+        if self.is_('ftp'):
             self.connection = FTP(location)
             self.connection .login(user, pwd)
 
     def send(self, filepath, filename, output_file, create_patch=None):
-        if self.protocole.lower() == 'ftp':
+        if self.is_('ftp'):
             self.connection.cwd(filepath)
             self.connection.storbinary('STOR ' + filename, output_file)
             output_file.close()
             return True
+        elif self.is_('filestore'):
+            output = open(os.path.join(filepath, filename), 'w+b')
+            for line in output_file.readlines():
+                output.write(line)
+            output.close()
+            output_file.close()
+            return True
 
     def get(self, filepath, filename):
-        if self.protocole.lower() == 'ftp':
-
-            #outfile = TemporaryFile('w+b')
-            outfile = open("/tmp/testalacon", 'w+b')
+        if self.is_('ftp'):
+            outfile = TemporaryFile('w+b')
             self.connection.cwd('/') #go to root menu by security
             self.connection.cwd(filepath)
             self.connection.retrbinary("RETR " + filename, outfile.write)
             outfile.seek(0)
             return outfile
+        if self.is_('filestore'):
+            return open(os.path.join(filepath, filename), 'w+b')
 
     def search(self, filepath, filename):
-        if self.protocole.lower() == 'ftp':
+        if self.is_('ftp'):
             self.connection.cwd('/') #go to root menu by security
             self.connection.cwd(filepath)
             return [x for x in self.connection.nlst() if filename.encode('utf-8') in x]
+        if self.is_('filestore'):
+            return [x for x in os.listdir(filepath) if filename.encode('utf-8') in x]
+
+

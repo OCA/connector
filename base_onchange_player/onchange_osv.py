a51808418fcefe9170f739036cf286ac98354fa1
sebastien beau <sebastien.beau@akretion.com.br>
2012-02-29 20:58:19 +0100

    [IMP] base_onchange_player : finish onchange support for base_sale_mulichannels

--- base_onchange_player/onchange_osv.py
+++ base_onchange_player/onchange_osv.py
@@ -23,17 +23,23 @@ from osv import osv
 import netsvc
 from tools.translate import _
 
-def call_onchange(self, cr, uid, onchange_name, vals, **kwargs):
+def call_onchange(self, cr, uid, onchange_name, vals, defaults=None, **kwargs):
     """
     Used in base_sale_multichannel in order to call onchange method on sale_order_line and sale_order
     In order to call onchange you must have to create a function "_get_kwargs_my_onchange_name"
     that will return the kwargs for your onchange
 
     @param onchange_name: string that contain the onchange method to call
+    @param vals: dictionnary of value that have been filled for the object
+    @param defaults: dictionnary of defaults values for the object
     @return: dictionary of line updated with the value returned by the onchange
     """
+    if not defaults:
+        defaults={}
+    vals_with_default = defaults.copy()
+    vals_with_default.update(vals)
     try :
-        kwargs2 = eval("self._get_kwargs_%s"%onchange_name)(cr, uid, vals, **kwargs)
+        kwargs2 = eval("self._get_kwargs_%s"%onchange_name)(cr, uid, vals_with_default, **kwargs)
     except Exception, e:
         raise osv.except_osv(_('On Change Player'), _("Error when trying to get the kwargs for the onchange %s on \
                                                     the object %s. Error message : %s" %(onchange_name, self._name, e)))
@@ -41,7 +47,11 @@ def call_onchange(self, cr, uid, onchange_name, vals, **kwargs):
         res = eval("self.%s"%onchange_name)(cr, uid, **kwargs2)
         for key in res['value']:
             if not key in vals:
-                vals[key] = res['value'][key]
+                # If the value is false and the field is not a boolean we don't pass it as it useless
+                # If it's a problem for you please contact me sebastien.beau@akretion.com, 
+                # because pass empty value will provoque some conflict with magentoerpconnect. Thanks
+                if res['value'][key] or self._columns[key]._type == 'bool':
+                    vals[key] = res['value'][key]
     except Exception, e:
         raise osv.except_osv(_('On Change Player'), _("Error when trying to playing the onchange %s on the object %s. \
                                                                 Error message : %s" %(onchange_name, self._name, e)))

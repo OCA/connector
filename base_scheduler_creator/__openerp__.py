fd2a9292e0ff81e3f3375e92cf6fa0b8c10596a5
Alexis de Lattre <alexis@via.ecp.fr>
2012-05-19 18:31:30 +0200

    [IMP] base_scheduler_creator clean code

--- base_scheduler_creator/__openerp__.py
+++ base_scheduler_creator/__openerp__.py
@@ -1,34 +1,36 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 #########################################################################
-#                                                                       #
-# Copyright (C) 2010  Sébastien Beau                                    #
-#                                                                       #
-#This program is free software: you can redistribute it and/or modify   #
-#it under the terms of the GNU General Public License as published by   #
-#the Free Software Foundation, either version 3 of the License, or      #
-#(at your option) any later version.                                    #
-#                                                                       #
-#This program is distributed in the hope that it will be useful,        #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of         #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
-#GNU General Public License for more details.                           #
-#                                                                       #
-#You should have received a copy of the GNU General Public License      #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#
+#    Base scheduler creator module for OpenERP
+#    Copyright (C) 2010  Sébastien Beau <sebastien.beau@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 #########################################################################
+
 {
     "name" : "base_scheduler_creator",
     "version" : "1.0",
-    "depends" : [],
-    "author" : "Sébastien Beau",
-    "description": """Automatique Scheduler Creator
-""",
+    "license": "AGPL-3",
+    "depends" : ["base"],
+    "author" : "Akretion",
+    "description": """Allows the automatic creation of a scheduler.""",
     "website" : "http://www.akretion.com/",
-    "category" : "Customer Modules",
+    "category" : "Generic Modules/Other",
     "init_xml" : [],
     "demo_xml" : [],
     "update_xml" : ["schedulder_creator.xml"],
     "active": False,
     "installable": True,
-
 }
--- base_scheduler_creator/schedulder_creator.py
+++ base_scheduler_creator/schedulder_creator.py
@@ -1,34 +1,35 @@
 # -*- encoding: utf-8 -*-
 #########################################################################
-#                                                                       #
-# Copyright (C) 2010   Sébastien Beau                                   #
-#                                                                       #
-#This program is free software: you can redistribute it and/or modify   #
-#it under the terms of the GNU General Public License as published by   #
-#the Free Software Foundation, either version 3 of the License, or      #
-#(at your option) any later version.                                    #
-#                                                                       #
-#This program is distributed in the hope that it will be useful,        #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of         #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
-#GNU General Public License for more details.                           #
-#                                                                       #
-#You should have received a copy of the GNU General Public License      #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#
+#    Base scheduler creator module for OpenERP
+#    Copyright (C) 2010  Sébastien Beau <sebastien.beau@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 #########################################################################
 
 from osv import fields,osv
-import os
 from tools.translate import _
 
-class scheduler_creator_wizard(osv.osv):
+class scheduler_creator_wizard(osv.osv_memory):
     _name = 'scheduler.creator.wizard'
     _description = 'scheduler creator wizard'
     
     def action_create(self, cr, uid, id, context):
         for id in context['active_ids']:
             if context.get('object_link', False) and self.pool.get(context['object_link']).read(cr, uid, id, ['scheduler'], context)['scheduler']:
-                raise osv.except_osv(_('USER ERROR'), _('A scheduler already exist!!'))
+                raise osv.except_osv(_('USER ERROR'), _('A scheduler already exists !'))
             
             vals = {'name':self.pool.get(context['object_link']).read(cr, uid, id, ['name'], context)['name'],
                     'active':False,
@@ -44,6 +45,6 @@ class scheduler_creator_wizard(osv.osv):
             cron_id = self.pool.get('ir.cron').create(cr, uid, vals, context)
             if context.get('object_link', False):
                 self.pool.get(context['object_link']).write(cr, uid, id, {'scheduler' : cron_id}, context)
-        return {'type': 'ir.actions.act_window_close'}  
+        return {'type': 'ir.actions.act_window_close'}
 
 scheduler_creator_wizard()

0d8c0117789ff38155b65883b23be41ac91229f6
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-02-19 17:54:26 +0100

    changed imports to be able to import from connector

--- connector/unit/__init__.py
+++ connector/unit/__init__.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from mapper import *
-from binder import *
-from backend_adapter import *
-from synchronizer import *
-
-
+from .mapper import *
+from .binder import *
+from .backend_adapter import *
+from .synchronizer import *

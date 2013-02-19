869b0109ad89a7389bda0414fcdbd328392f6804
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-02-19 10:57:46 +0100

    [ADD] common methods for import and export synchronizers

--- base_external_referentials/unit/synchronizer.py
+++ base_external_referentials/unit/synchronizer.py
@@ -20,6 +20,9 @@
 ##############################################################################
 
 from ..connector import ConnectorUnit
+from .binder import Binder
+from .mapper import Mapper
+from .backend_adapter import BackendAdapter
 
 
 class Synchronizer(ConnectorUnit):
@@ -28,10 +31,62 @@ class Synchronizer(ConnectorUnit):
     # implement in sub-classes
     _model_name = None
 
+    def run(self):
+        """ Run the synchronization """
+        raise NotImplementedError
 
-class ExportSynchronizer(Synchronizer):
+
+class ImportExportSynchronizer(Synchronizer):
+    """ Common base class for import and exports """
+
+    def __init__(self, reference, session):
+        super(ImportExportSynchronizer, self).__init__(reference)
+        self.session = session
+        self.model = self.session.pool.get(self.model_name)
+
+        self._binder = None
+        self._external_adapter = None
+        self._processor = None
+
+    @property
+    def binder(self):
+        if self._binder is None:
+            self._binder = self.reference.get_class(
+                    Binder, self.model_name)(self.reference, self.session)
+        return self._binder
+
+    @binder.setter
+    def binder(self, binder):
+        self._binder = binder
+
+    @property
+    def backend_adapter(self):
+        if self._backend_adapter is None:
+            adapter_cls = self.reference.get_class(BackendAdapter,
+                                                   self.model_name)
+            self._backend_adapter = adapter_cls(self.reference, self.session)
+        return self._external_adapter
+
+    @backend_adapter.setter
+    def backend_adapter(self, backend_adapter):
+        self._backend_adapter = backend_adapter
+
+    @property
+    def mapper(self):
+        if self._mapper is None:
+            self._mapper = self.reference.get_class(
+                    Mapper, self.model_name)(self.reference, self.session)
+        return self._mapper
+
+    @mapper.setter
+    def mapper(self, mapper):
+        self._mapper = mapper
+
+
+
+class ExportSynchronizer(ImportExportSynchronizer):
     """ Synchronizer for exporting data from OpenERP to a backend """
 
 
-class ImportSynchronizer(Synchronizer):
+class ImportSynchronizer(ImportExportSynchronizer):
     """ Synchronizer for importing data from a backend to OpenERP """

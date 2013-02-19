c2019e0b0fb4f0c2252c08760829cabd4897c15d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-02-19 11:25:53 +0100

    [CHG] rename base_external_referentials to connector

---
+++ connector/external_osv.py
@@ -0,0 +1,1 @@
+from .deprecated.external_osv import *
---
+++ connector/producer.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Guewen Baconnier
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+"""
+Producers of events.
+
+Fire the common events:
+
+-  ``on_record_create`` when a record is created
+-  ``on_record_write`` when something is written on a record
+-  ``on_record_unlink``  when a record is deleted
+
+"""
+
+from openerp.osv import orm
+from .session import ConnectorSession
+from .event import (on_record_create,
+                    on_record_write,
+                    on_record_unlink)
+
+
+create_original = orm.Model.create
+def create(self, cr, uid, vals, context=None):
+    record_id = create_original(self, cr, uid, vals, context=context)
+    if on_record_create.has_consumer_for(self._name):
+        session = ConnectorSession(cr, uid, context=context)
+        on_record_create.fire(self._name, session, record_id)
+    return record_id
+orm.Model.create = create
+
+
+write_original = orm.Model.write
+def write(self, cr, uid, ids, vals, context=None):
+    result = write_original(self, cr, uid, ids, vals, context=context)
+    if not hasattr(ids, '__iter__'):
+        ids = [ids]
+    if on_record_write.has_consumer_for(self._name):
+        session = ConnectorSession(cr, uid, context=context)
+        for record_id in ids:
+            on_record_write.fire(self._name, session, record_id, vals.keys())
+    return result
+orm.Model.write = write
+
+
+unlink_original = orm.Model.unlink
+def unlink(self, cr, uid, ids, context=None):
+    result = unlink_original(self, cr, uid, ids, context=context)
+    if not hasattr(ids, '__iter__'):
+        ids = [ids]
+    if on_record_unlink.has_consumer_for(self._name):
+        session = ConnectorSession(cr, uid, context=context)
+        for record_id in ids:
+            on_record_unlink.fire(self._name, session, record_id)
+    return result
+orm.Model.unlink = unlink

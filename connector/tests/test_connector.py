cc5b50250b819ca31aac220df021141ffc3a31a6
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-02-20 16:34:46 +0100

    [FIX] test, the model is now a list

--- connector/tests/test_connector.py
+++ connector/tests/test_connector.py
@@ -13,7 +13,7 @@ class test_connector_unit(unittest2.TestCase):
         class ModelUnit(ConnectorUnit):
             _model_name = model
 
-        self.assertEqual(ModelUnit.model_name, model)
+        self.assertEqual(ModelUnit.model_name, [model])
 
     def test_connector_unit_no_model_name(self):
         with self.assertRaises(NotImplementedError):

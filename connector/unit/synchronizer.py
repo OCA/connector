e259289f5ae3911ae3f538e46169d7000cce0d9a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-02-21 17:17:58 +0100

    Binder, Mapper and BackendAdapter are now returned lately when they are asked for in the Synchronizer, because they are not necessarily used nor existing for certain synchronizations

--- connector/unit/synchronizer.py
+++ connector/unit/synchronizer.py
@@ -33,36 +33,85 @@ class Synchronizer(ConnectorUnit):
 
     def __init__(self, environment):
         super(Synchronizer, self).__init__(environment)
-        model_name = environment.model_name
-        get_class = self.backend.get_class
-        self.binder = get_class(Binder, model_name)(environment)
-        self.backend_adapter = get_class(BackendAdapter, model_name)(environment)
-        self.mapper = None
-        self._init_mapper(environment)
-
-    def _init_mapper(self, environment):
-        model_name = environment.model_name
-        get_class = self.backend.get_class
-        self.mapper = get_class(Mapper, model_name)(environment)
+        self._backend_adapter = None
+        self._binder = None
+        self._mapper = None
 
     def run(self):
         """ Run the synchronization """
         raise NotImplementedError
 
+    @property
+    def mapper(self):
+        """ Return an instance of ``Mapper`` for the synchronization.
+
+        The instanciation is delayed because some synchronisations do
+        not need such an unit and the unit may not exist.
+
+        :rtype: :py:class:`connector.unit.mapper.Mapper`
+        """
+        if self._mapper is None:
+            self._mapper = self.environment.get_connector_unit(Mapper)
+        return self._mapper
+
+    @property
+    def binder(self):
+        """ Return an instance of ``Binder`` for the synchronization.
+
+        The instanciation is delayed because some synchronisations do
+        not need such an unit and the unit may not exist.
+
+        :rtype: :py:class:`connector.unit.binder.Binder`
+        """
+        if self._binder is None:
+            self._binder = self.environment.get_connector_unit(Binder)
+        return self._binder
+
+    @property
+    def backend_adapter(self):
+        """ Return an instance of ``BackendAdapter`` for the
+        synchronization.
+
+        The instanciation is delayed because some synchronisations do
+        not need such an unit and the unit may not exist.
+
+        :rtype: :py:class:`connector.unit.backend_adapter.BackendAdapter`
+        """
+        if self._backend_adapter is None:
+            get_unit = self.environment.get_connector_unit
+            self._backend_adapter = get_unit(BackendAdapter)
+        return self._backend_adapter
+
 
 class ExportSynchronizer(Synchronizer):
     """ Synchronizer for exporting data from OpenERP to a backend """
 
-    def _init_mapper(self, environment):
-        model_name = environment.model_name
-        get_class = self.backend.get_class
-        self.mapper = get_class(ExportMapper, model_name)(environment)
+    @property
+    def mapper(self):
+        """ Return an instance of ``Mapper`` for the synchronization.
+
+        The instanciation is delayed because some synchronisations do
+        not need such an unit and the unit may not exist.
+
+        :rtype: :py:class:`connector.unit.mapper.ExportMapper`
+        """
+        if self._mapper is None:
+            self._mapper = self.environment.get_connector_unit(ExportMapper)
+        return self._mapper
 
 
 class ImportSynchronizer(Synchronizer):
     """ Synchronizer for importing data from a backend to OpenERP """
 
-    def _init_mapper(self, environment):
-        model_name = environment.model_name
-        get_class = self.backend.get_class
-        self.mapper = get_class(ImportMapper, model_name)(environment)
+    @property
+    def mapper(self):
+        """ Return an instance of ``Mapper`` for the synchronization.
+
+        The instanciation is delayed because some synchronisations do
+        not need such an unit and the unit may not exist.
+
+        :rtype: :py:class:`connector.unit.mapper.ImportMapper`
+        """
+        if self._mapper is None:
+            self._mapper = self.environment.get_connector_unit(ImportMapper)
+        return self._mapper

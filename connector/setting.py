e52f67fceb272befd404049c773fb84f0031625b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-03-05 14:30:12 +0100

    missing files from the previous commit

---
+++ connector/setting.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Guewen Baconnier
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+class note_base_config_settings(orm.TransientModel):
+
+    _name = 'connector.config.settings'
+    _inherit = 'base.config.settings'
+
+    _columns = {
+        'module_magentoerpconnect': fields.boolean(
+            "Synchronize with Magentoerpconnect",
+            help="""This installs the module magentoerpconnect."""),
+    }

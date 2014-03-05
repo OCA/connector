f511b071b3048d373a0f318e8b1e02dd8d487e9d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-03-05 17:46:40 +0100

    migrate the text field containing the pickled function to a bytea field

---
+++ connector/migrations/2.2.0/pre-migration.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+
+__name__ = "Convert jobs pickled func to bytea"
+
+def migrate(cr, version):
+    if not version:
+        return
+    cr.execute("ALTER TABLE queue_job ALTER func "
+               "TYPE bytea USING func::bytea ")

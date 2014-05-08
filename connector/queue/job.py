5c3a0f0c8f703bb24eaac2ff1b2b9edf7dd3fdbb
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-05-08 17:05:37 +0200

    add tests

--- connector/queue/job.py
+++ connector/queue/job.py
@@ -600,6 +600,8 @@ class Job(object):
             self.result = result
 
     def related_action(self, session):
+        if not hasattr(self.func, 'related_action'):
+            return None
         return self.func.related_action(session, self)
 
 
@@ -668,9 +670,6 @@ def job(func):
             *args,
             **kwargs)
     func.delay = delay
-    # set a default related action
-    if not hasattr(func, 'related_action'):
-        func = related_action()(func)
     return func
 
 

5f725c689952ba3861f8533292b9e97ae50d27bd
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-05-09 11:07:42 +0200

    add a generic implementation of related_action that opens a form by unwrapping a binding record

--- connector/tests/test_related_action.py
+++ connector/tests/test_related_action.py
@@ -5,12 +5,13 @@ import unittest2
 
 import openerp
 import openerp.tests.common as common
-from openerp.addons.connector.queue.job import (
-    Job,
-    OpenERPJobStorage,
-    related_action)
-from openerp.addons.connector.session import (
-    ConnectorSession)
+from ..backend import Backend, BACKENDS
+from ..connector import Binder
+from ..queue.job import (Job,
+                         OpenERPJobStorage,
+                         related_action)
+from ..session import ConnectorSession
+from ..related_action import unwrap_binding
 
 
 def task_no_related(session, model_name):
@@ -40,6 +41,11 @@ def open_url(session, job, url=None):
 
 @related_action(action=open_url, url='https://en.wikipedia.org/wiki/{subject}')
 def task_wikipedia(session, subject):
+    pass
+
+
+@related_action(action=unwrap_binding)
+def test_unwrap_binding(session, model_name, binding_id):
     pass
 
 
@@ -77,6 +83,39 @@ class test_related_action(unittest2.TestCase):
         job_func = related_action(action=action, b=4)(task_related_return_kwargs)
         job = Job(func=job_func)
         self.assertEqual(job.related_action(self.session), (1, 4))
+
+    def test_unwrap_binding(self):
+        """ Call the unwrap binding related action """
+        class TestBinder(Binder):
+            _model_name = 'binding.res.users'
+
+            def unwrap_record(self, binding_id, browse=False):
+                return 42
+
+            def unwrap_model(self):
+                return 'res.users'
+
+        job = Job(func=test_unwrap_binding, args=('res.users', 555))
+        session = mock.Mock(name='session')
+        backend_record = mock.Mock(name='backend_record')
+        backend = mock.Mock(name='backend')
+        browse_record = mock.Mock(name='browse_record')
+        backend.get_class.return_value = TestBinder
+        backend_record.get_backend.return_value = backend
+        browse_record.exists.return_value = True
+        browse_record.backend_id = backend_record
+        session.browse.return_value = browse_record
+        action = unwrap_binding(session, job)
+        expected = {
+            'name': mock.ANY,
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_id': 42,
+            'res_model': 'res.users',
+        }
+        self.assertEquals(action, expected)
+
 
 
 class test_related_action_storage(common.TransactionCase):

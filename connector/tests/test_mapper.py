8100f7af32d43436e86cc76fc6bd802ca9312c0c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-05-23 16:21:14 +0200

    extract the attribute name from the closure when a modifier is used in a 'direct' mapping

--- connector/tests/test_mapper.py
+++ connector/tests/test_mapper.py
@@ -558,3 +558,19 @@ class test_mapper_binding(common.TransactionCase):
                                           'test': .5})]
                     }
         self.assertEqual(map_record.values(for_create=True), expected)
+
+    def test_modifier_filter_field(self):
+        """ A direct mapping with a modifier must still be considered from the list of fields """
+        class MyMapper(ImportMapper):
+            direct = [('field', 'field2'),
+                      ('no_field', 'no_field2'),
+                      (convert('name', int), 'out_name')]
+
+        env = mock.MagicMock()
+        record = {'name': '300', 'field': 'value', 'no_field': 'no_value'}
+        mapper = MyMapper(env)
+        map_record = mapper.map_record(record)
+        expected = {'out_name': 300, 'field2': 'value'}
+        self.assertEqual(map_record.values(fields=['field', 'name']), expected)
+        self.assertEqual(map_record.values(for_create=True,
+                                           fields=['field', 'name']), expected)

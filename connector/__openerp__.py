d24ada9deb0e69a3776434bb4d9d60e83022f462
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-03-20 09:17:38 +0100

    Set version 3 in __openerp__

--- connector/__openerp__.py
+++ connector/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {'name': 'Connector',
- 'version': '2.2.0',
+ 'version': '3.0.0',
  'author': 'Camptocamp,Openerp Connector Core Editors,'
            'Odoo Community Association (OCA)',
  'website': 'http://odoo-connector.com',

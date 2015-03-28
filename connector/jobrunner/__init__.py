247385c7864a5cca8ee52da6328e1354a88f671c
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-03-28 21:56:05 +0100

    [WIP] odoo-connector-runner: in-process runner thread should now be in good shape

--- connector/jobrunner/__init__.py
+++ connector/jobrunner/__init__.py
@@ -1,11 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#     This file is part of connector, an Odoo module.
+#
+#     Author: Stéphane Bidoul <stephane.bidoul@acsone.eu>
+#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
+#
+#     connector is free software: you can redistribute it and/or
+#     modify it under the terms of the GNU Affero General Public License
+#     as published by the Free Software Foundation, either version 3 of
+#     the License, or (at your option) any later version.
+#
+#     connector is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the
+#     GNU Affero General Public License
+#     along with connector.
+#     If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 import logging
+import os
 from threading import Thread
 import time
 
-from openerp.service import db
 from openerp.service import server
+from openerp.tools import config
 
-from . import channels
 from .runner import OdooConnectorRunner
 
 _logger = logging.getLogger(__name__)
@@ -13,16 +38,26 @@ _logger = logging.getLogger(__name__)
 START_DELAY = 10
 
 
+# Here we monkey patch the Odoo server to start the job runner thread
+# in the main server process (and not in forked workers). This is
+# very easy to deploy as we don't need another startup script.
+# The drawback is that it is not possible to extend the Odoo
+# server command line arguments, so we resort to environment variables
+# to configure the runner (channels mostly).
+
+
+# TODO: this is a temporary flag to enable the connector runner
+enable = os.environ.get('ODOO_CONNECTOR_RUNNER_ENABLE')
+
+
 def run():
     # sleep a bit to let the workers start at ease
     time.sleep(START_DELAY)
-    _logger.info("dbs: %s", db.exp_list())
-    OdooConnectorRunner().run_forever()
+    port = os.environ.get('ODOO_CONNECTOR_PORT') or config['xmlrpc_port']
+    channels = os.environ.get('ODOO_CONNECTOR_CHANNELS')
+    runner = OdooConnectorRunner(port or 8069, channels or 'root:1')
+    runner.run_forever()
 
-
-# monkey patch the Odoo server to start the job runner thread
-# once in the main process and in the main process only
-# (ie not in forked workers)
 
 orig_prefork_start = server.PreforkServer.start
 orig_threaded_start = server.ThreadedServer.start
@@ -31,27 +66,30 @@ orig_gevent_start = server.GeventServer.start
 
 def prefork_start(server, *args, **kwargs):
     res = orig_prefork_start(server, *args, **kwargs)
-    _logger.error("jobrunner prefork start")
-    thread = Thread(target=run)
-    thread.daemon = True
-    thread.start()
+    if enable and not config['stop_after_init']:
+        _logger.info("starting jobrunner thread (in prefork server)")
+        thread = Thread(target=run)
+        thread.daemon = True
+        thread.start()
     return res
 
 
 def threaded_start(server, *args, **kwargs):
     res = orig_threaded_start(server, *args, **kwargs)
-    _logger.error("jobrunner threaded start")
-    thread = Thread(target=run)
-    thread.daemon = True
-    thread.start()
+    if enable and not config['stop_after_init']:
+        _logger.info("starting jobrunner thread (in threaded server)")
+        thread = Thread(target=run)
+        thread.daemon = True
+        thread.start()
     return res
 
 
 def gevent_start(server, *args, **kwargs):
     res = orig_gevent_start(server, *args, **kwargs)
-    _logger.error("jobrunner gevent start")
-    # TODO: gevent spawn?
-    raise RuntimeError("not implemented")
+    if enable and not config['stop_after_init']:
+        _logger.info("starting jobrunner thread (in gevent server)")
+        # TODO: gevent spawn?
+        raise RuntimeError("not implemented")
     return res
 
 

58acaf95da317da9ba7907e255d3d9c0299a6147
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-09-28 11:35:28 +0200

    [IMP] answer another jobrunner FAQ in the documentation

--- connector/jobrunner/runner.py
+++ connector/jobrunner/runner.py
@@ -84,6 +84,22 @@ Caveat
 * After creating a new database or installing connector on an
   existing database, Odoo must be restarted for the runner to detect it.
 
+* When Odoo shuts down normally, it waits for running jobs to finish.
+  However, when the Odoo server crashes or is otherwise force-stopped,
+  running jobs are interrupted while the runner has no chance to know
+  they have been aborted. In such situations, jobs may remain in
+  ``started`` or ``enqueued`` state after the Odoo server is halted.
+  Since the runner has no way to know if they are actually running or
+  not, and does not know for sure if it is safe to restart the jobs,
+  it does not attempt to restart them automatically. Such stale jobs
+  therefore fill the running queue and prevent other jobs to start.
+  You must therefore requeue them manually, either from the Jobs view,
+  or by running the following SQL statement *before starting Odoo*:
+
+.. code-block:: sql
+
+  update queue_job set state='pending' where state in ('started', 'enqueued')
+
 .. rubric:: Footnotes
 
 .. [1] From a security standpoint, it is safe to have an anonymous HTTP

74d77a185405e740e4879149300994753544a24e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:47 +0200

    [UPD] prefix versions with 8.0

--- connector/__openerp__.py
+++ connector/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {'name': 'Connector',
- 'version': '3.2.0',
+ 'version': '8.0.3.2.0',
  'author': 'Camptocamp,Openerp Connector Core Editors,'
            'Odoo Community Association (OCA)',
  'website': 'http://odoo-connector.com',

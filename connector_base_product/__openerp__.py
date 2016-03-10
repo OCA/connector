3f7592965027e061750d57b793bd5c1e8f0f4f2a
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-03-10 20:41:16 +0530

    [PORT][9.0] connector_base_product

--- connector_base_product/__openerp__.py
+++ connector_base_product/__openerp__.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: David BEAL, Copyright Akretion, 2014
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2014 David BEAL Akretion, Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {'name': 'Connector Base Product',
- 'version': '8.0.1.0.0',
- 'author': "Openerp Connector Core Editors,Odoo Community Association (OCA)",
+ 'version': '9.0.1.0.0',
+ 'author': "Openerp Connector Core Editors, Odoo Community Association (OCA)",
  'website': 'http://odoo-connector.com',
  'license': 'AGPL-3',
  'category': 'Connector',
@@ -35,7 +19,7 @@ Add 'Connector' tab to product view
      'product',
  ],
  'data': [
-     'product_view.xml'
+     'views/product_view.xml'
  ],
- 'installable': False,
+ 'installable': True,
  }

17e257150e71dfffed0480b6149a5135246726a9
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-10-03 14:18:28 +0200

    Replace monkey-patch by a proper inherit
    Now that we have a root model 'base', we can properly override the
    methods to fire the events.

--- connector/producer.py
+++ connector/producer.py
@@ -30,8 +30,7 @@ Fire the common events:
 
 """
 
-import odoo
-from odoo import models
+from odoo import api, models
 from .session import ConnectorSession
 from .event import (on_record_create,
                     on_record_write,
@@ -39,48 +38,36 @@ from .event import (on_record_create,
 from .connector import is_module_installed
 
 
-create_original = models.BaseModel.create
+class Base(models.AbstractModel):
+    """ The base model, which is implicitly inherited by all models. """
+    _inherit = 'base'
 
+    @api.model
+    @api.returns('self', lambda value: value.id)
+    def create(self, vals):
+        record = super(Base, self).create(vals)
+        if is_module_installed(self.env, 'connector'):
+            session = ConnectorSession.from_env(self.env)
+            on_record_create.fire(session, self._name, record.id, vals)
+        return record
 
-@odoo.api.model
-@odoo.api.returns('self', lambda value: value.id)
-def create(self, vals):
-    record_id = create_original(self, vals)
-    if is_module_installed(self.env, 'connector'):
-        session = ConnectorSession(self.env.cr, self.env.uid,
-                                   context=self.env.context)
-        on_record_create.fire(session, self._name, record_id.id, vals)
-    return record_id
-models.BaseModel.create = create
+    @api.multi
+    def write(self, vals):
+        result = super(Base, self).write(vals)
+        if is_module_installed(self.env, 'connector'):
+            session = ConnectorSession.from_env(self.env)
+            if on_record_write.has_consumer_for(session, self._name):
+                for record_id in self.ids:
+                    on_record_write.fire(session, self._name,
+                                         record_id, vals)
+        return result
 
-
-write_original = models.BaseModel.write
-
-
-@odoo.api.multi
-def write(self, vals):
-    result = write_original(self, vals)
-    if is_module_installed(self.env, 'connector'):
-        session = ConnectorSession(self.env.cr, self.env.uid,
-                                   context=self.env.context)
-        if on_record_write.has_consumer_for(session, self._name):
-            for record_id in self.ids:
-                on_record_write.fire(session, self._name,
-                                     record_id, vals)
-    return result
-models.BaseModel.write = write
-
-
-unlink_original = models.BaseModel.unlink
-
-
-@odoo.api.multi
-def unlink(self):
-    if is_module_installed(self.env, 'connector'):
-        session = ConnectorSession(self.env.cr, self.env.uid,
-                                   context=self.env.context)
-        if on_record_unlink.has_consumer_for(session, self._name):
-            for record_id in self.ids:
-                on_record_unlink.fire(session, self._name, record_id)
-    return unlink_original(self)
-models.BaseModel.unlink = unlink
+    @api.multi
+    def unlink(self):
+        result = super(Base, self).unlink()
+        if is_module_installed(self.env, 'connector'):
+            session = ConnectorSession.from_env(self.env)
+            if on_record_unlink.has_consumer_for(session, self._name):
+                for record_id in self.ids:
+                    on_record_unlink.fire(session, self._name, record_id)
+        return result

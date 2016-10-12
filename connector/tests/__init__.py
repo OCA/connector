806d1e918d6b78de06fcc10ce544ad309b693af0
Simone Orsi <simahawk@gmail.com>
2016-10-12 10:37:16 +0200

    [fix] tests

--- connector/tests/__init__.py
+++ connector/tests/__init__.py
@@ -30,3 +30,4 @@ from . import test_related_action
 from . import test_runner_channels
 from . import test_runner_runner
 from . import test_checkpoint
+from . import test_default_binder
--- connector/tests/test_default_binder.py
+++ connector/tests/test_default_binder.py
@@ -37,19 +37,28 @@ class TestDefaultBinder(TransactionCase):
         partner.write({'color': 1})
         # bind the main partner to external id = 0
         self.partner_binder.bind(0, partner.id)
+
         # find the openerp partner bound to external partner 0
-        openerp_id = self.partner_binder.to_openerp(0)
-        self.assertEqual(openerp_id, partner.id)
-        openerp_id = self.partner_binder.to_openerp(0)
-        self.assertEqual(openerp_id.id, partner.id)
-        openerp_id = self.partner_binder.to_openerp(0, unwrap=True)
-        self.assertEqual(openerp_id, partner.id)
+        binding = self.partner_binder.to_openerp(0)
+        self.assertEqual(binding.id, partner.id)
+        binding_id = self.partner_binder.to_openerp(0, unwrap=True)
+        self.assertEqual(binding_id, partner.id)
+
+        # we have `_external_field = 'ref'`
+        partner.ref = 'foo'
         # find the external partner bound to openerp partner 1
-        external_id = self.partner_binder.to_backend(partner.id)
-        self.assertEqual(external_id, '0')
-        external_id = self.partner_binder.to_backend(partner.id, wrap=True)
-        self.assertEqual(external_id, '0')
+        backend_id = self.partner_binder.to_backend(partner.id)
+        self.assertEqual(backend_id, 'foo')
+        backend_id = self.partner_binder.to_backend(binding_id, wrap=True)
+        self.assertEqual(backend_id, 'foo')
+
         # unwrap model should be None since we set 'id' as the _openerp_field
         self.assertEqual(self.partner_binder.unwrap_model(), None)
-        # unwrapping the binding should give the same binding
-        self.assertEqual(self.partner_binder.unwrap_binding(1), 1)
+        # unwrapping the binding should give the same binding.
+        self.assertEqual(self.partner_binder.unwrap_binding(1, browse=True), 1)
+        # Yes, we are supposed to have a browser record back BUT
+        # we are faking the binder to play without any real binding model
+        # (see that we have `_openerp_field = 'id'`)
+        # To test all this stuff properly we should have custom test models,
+        # but on Odoo you need to create a separate module for that
+        # and move the tests there.

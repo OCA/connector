634d05cc080d86c1ed760017f240c745e801a9d8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-01-12 16:48:21 +0100

    Add a test_connector addon
    So we can write tests using real pieces of the framework on real models
    instead of using mocks which is quite limited and wring things in
    confusing ways.
    
    * Fix the related action implementation
    * Move the tests of the Default Binding in this module so we have tests
      that reflect the reality

---
+++ test_connector/README.rst
@@ -0,0 +1,63 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==============
+Test Connector
+==============
+
+This addon is not meant to be used. It extends the Odoo Models
+in order to run automated tests on the Connector framework
+
+The basic tests are integrated within the ``connector`` addon.
+
+Installation
+============
+
+Nothing particular.
+
+Usage
+=====
+
+This module only contains Python tests.
+
+Known issues / Roadmap
+======================
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/connector/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Images
+------
+
+* Odoo Community Association: `Icon <https://github.com/OCA/maintainer-tools/blob/master/template/module/static/description/icon.svg>`_.
+
+Contributors
+------------
+
+* Guewen Baconnier <guewen.baconnier@camptocamp.com>
+* Simone Orsi <simone.orsi@camptocamp.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
+
---
+++ test_connector/tests/test_related_action_binding.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import mock
+
+from odoo.exceptions import UserError
+import odoo.tests.common as common
+
+
+class TestRelatedActionBinding(common.TransactionCase):
+    """ Test Related Actions with Bindings """
+
+    def setUp(self):
+        super(TestRelatedActionBinding, self).setUp()
+
+        self.backend_record = self.env['test.backend'].create(
+            {'version': '1', 'name': 'Test'}
+        )
+
+    def test_unwrap_binding(self):
+        """ Call the unwrap binding related action """
+        binding = self.env['connector.test.binding'].create({
+            'backend_id': self.backend_record.id,
+            'external_id': 99,
+        })
+
+        job = binding.with_delay().job_related_action_unwrap()
+        db_job = job.db_record()
+        action = db_job.open_related_action()
+        expected = {
+            'name': mock.ANY,
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_id': binding.odoo_id.id,
+            'res_model': 'connector.test.record',
+        }
+        self.assertEquals(action, expected)
+
+    def test_unwrap_binding_direct_binding(self):
+        """ Call the unwrap binding related action """
+        binding = self.env['no.inherits.binding'].create({
+            'backend_id': self.backend_record.id,
+            'external_id': 99,
+        })
+
+        job = binding.with_delay().job_related_action_unwrap()
+        db_job = job.db_record()
+        action = db_job.open_related_action()
+        expected = {
+            'name': mock.ANY,
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_id': binding.id,
+            'res_model': 'no.inherits.binding',
+        }
+        self.assertEquals(action, expected)
+
+    def test_unwrap_binding_not_exists(self):
+        """ Call the related action on the model on non-existing record """
+        binding = self.env['connector.test.binding'].create({
+            'backend_id': self.backend_record.id,
+            'external_id': 99,
+        })
+
+        job = binding.with_delay().job_related_action_unwrap()
+
+        db_job = job.db_record()
+
+        binding.unlink()
+
+        with self.assertRaisesRegexp(UserError,
+                                     'No action available for this job'):
+            db_job.open_related_action()

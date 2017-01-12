64fddfba8dc8b30af890bc6fca9df93692b27126
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-01-12 23:05:15 +0100

    Start to implement the collection system

--- connector/components/base_components.py
+++ connector/components/base_components.py
@@ -3,10 +3,39 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from .core import Component
+from .collection import collection_registry
 
 
 class BaseComponent(Component):
     _name = 'base'
+
+    # TODO: add __init__ with 'connector_env'
+
+
+class Collection(Component):
+    """ Find components in the Collection Registry """
+
+    _name = 'collection'
+
+    def find(self, name=None, purpose=None, model_name=None):
+        # replace 'test.backend' by self.connector_env.backend_record._name
+        collection_registry.find(
+            'test.backend',
+            name=name,
+            purpose=purpose,
+            model_name=model_name,
+            multi=False,
+        )
+
+    def find_all(self, name=None, purpose=None, model_name=None):
+        # replace 'test.backend' by self.connector_env.backend_record._name
+        collection_registry.find(
+            'test.backend',
+            name=name,
+            purpose=purpose,
+            model_name=model_name,
+            multi=True,
+        )
 
 
 class Mapper(Component):

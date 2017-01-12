c5f217a34ff8136f481301defaa55e340676cc1b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-01-12 16:48:21 +0100

    Add a test_connector addon
    So we can write tests using real pieces of the framework on real models
    instead of using mocks which is quite limited and wring things in
    confusing ways.
    
    * Fix the related action implementation
    * Move the tests of the Default Binding in this module so we have tests
      that reflect the reality

---
+++ test_connector/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Connector Tests',
+ 'summary': 'Automated tests for Connector, do not install.',
+ 'version': '10.0.1.0.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': ['connector',
+             ],
+ 'website': 'http://www.camptocamp.com',
+ 'data': ['security/ir.model.access.csv',
+          ],
+ 'installable': True,
+ }

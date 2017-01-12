e2974c7ab6bb2c237c2cbdd41eb91393becf2df3
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-01-12 23:05:15 +0100

    Start to implement the collection system

--- connector/components/core.py
+++ connector/components/core.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
+# Copyright 2017 Odoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from collections import defaultdict

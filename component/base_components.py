2a372f279fd9c7d044758714dec9642639135ef4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-01-15 22:24:44 +0100

    Extract 'components' into an individual addon

---
+++ component/base_components.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from .core import Component
+
+
+class BaseComponent(Component):
+    _name = 'base'

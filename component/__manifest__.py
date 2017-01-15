9ccbb25c43ba3a7fb682a5d273d8e478af618fe4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-01-15 22:24:44 +0100

    Extract 'components' into an individual addon

---
+++ component/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Components',
+ 'version': '10.0.1.0.0',
+ 'author': 'Camptocamp,'
+           'Odoo Community Association (OCA)',
+ 'website': 'https://www.camptocamp.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': ['base',
+             ],
+ 'data': [],
+ 'installable': True,
+ }

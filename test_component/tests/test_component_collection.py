83d72e36fef0c897bd054a717cd7b175f4f58a2a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-01-19 21:40:38 +0100

    Separate lookup by name and by usage
    The favorite lookup should be by 'usage' (kinda interface) and by model
    name ('components()' method).  The lookup by component name should
    normally not be used as it reduces the flexibility.  Using a different
    method for this lookup discourage its usage.  Also, the lookup by
    component name completely ignores the current collection, which is bad
    or not bad depending of what you want to achieve.

--- test_component/tests/test_component_collection.py
+++ test_component/tests/test_component_collection.py
@@ -3,21 +3,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo.tests import common
-from odoo.addons.test_component.components.components import TestUserComponent
+from odoo.addons.test_component.components.components import UserTestComponent
 
 
 class TestComponentCollection(common.TransactionCase):
 
     def setUp(self):
         super(TestComponentCollection, self).setUp()
+        self.collection = self.env['test.component.collection'].create(
+            {'name': 'Test'}
+        )
 
     def tearDown(self):
         super(TestComponentCollection, self).tearDown()
 
-    def test_register_component(self):
-        collection = self.env['test.component.collection'].create(
-            {'name': 'Test'}
-        )
-        work = collection.work_on('res.users')
-        component = work.components(name='test.user.component')
-        self.assertEquals(TestUserComponent._name, component._name)
+    def test_component_by_name(self):
+        work = self.collection.work_on('res.users')
+        component = work.component_by_name(name='test.user.component')
+        self.assertEquals(UserTestComponent._name, component._name)
+
+    def test_components_usage(self):
+        work = self.collection.work_on('res.users')
+        component = work.components(usage='test1')
+        self.assertEquals(UserTestComponent._name, component._name)

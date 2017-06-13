013455e55ac4dc5cfdaee4b78e28ae914b413783
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-13 13:10:14 +0200

    Add AbstractComponent

--- component/base_components.py
+++ component/base_components.py
@@ -2,8 +2,8 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from .core import Component
+from .core import AbstractComponent
 
 
-class BaseComponent(Component):
+class BaseComponent(AbstractComponent):
     _name = 'base'

b1f7fb45f04be760630c97dcdc40d3de683b02a4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-15 21:18:53 +0200

    Document component

--- component/base_components.py
+++ component/base_components.py
@@ -6,4 +6,10 @@ from .core import AbstractComponent
 
 
 class BaseComponent(AbstractComponent):
+    """ This is the base component for every component
+
+    It is implicitely inherited by all components.
+
+    All your base are belong to us
+    """
     _name = 'base'

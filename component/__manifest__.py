ffa64f000c58b6181c9c9e95d9e0d6d367d293e1
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-17 22:20:49 +0200

    Draft new component_event addon
    Proposing a new API based on components for the events

--- component/__manifest__.py
+++ component/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013-2017 Camptocamp SA
+# Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {'name': 'Components',

7ab88f84afe94635965f6e1adf064f0eb94e729b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-20 09:35:09 +0200

    Move base component in a components directory
    For consistency, this is where components should go (as for models,
    views, ...)

--- component/components/base.py
+++ component/components/base.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from .core import AbstractComponent
+from ..core import AbstractComponent
 
 
 class BaseComponent(AbstractComponent):

b05c1aa7953bc131b39f07ec1386ca61fdabc218
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-22 21:58:10 +0200

    Change Collection.work_on() to a context manager
    It allows to open/close objects on start/end of a synchro / work
    session. The base method does none of that, but this is a really common
    use case. Example: at the beginning of the work session, I open a
    webservice client that is available in our WorkContext for the duration
    of our work. At the end, I close the client to end the connection.

--- test_connector/tests/test_default_binder.py
+++ test_connector/tests/test_default_binder.py
@@ -19,33 +19,35 @@ class TestDefaultBinder(TransactionCase):
         self.backend_record = self.env['test.backend'].create(
             {'version': '1', 'name': 'Test'}
         )
-        # create a work context with the model we want to work with
-        self.work = self.backend_record.work_on('connector.test.binding')
-        # get our binder component (for the model in whe work context)
-        self.binder = self.work.component(usage='binder')
 
     def test_default_binder(self):
         """ Small scenario with the default binder """
-        test_record = self.env['connector.test.record'].create({})
-        test_binding = self.env['connector.test.binding'].create({
-            'backend_id': self.backend_record.id,
-            'odoo_id': test_record.id,
-        })
+        # create a work context with the model we want to work with
+        with self.backend_record.work_on('connector.test.binding') as work:
+            # get our binder component (for the model in whe work context)
+            self.binder = work.component(usage='binder')
+            test_record = self.env['connector.test.record'].create({})
+            test_binding = self.env['connector.test.binding'].create({
+                'backend_id': self.backend_record.id,
+                'odoo_id': test_record.id,
+            })
 
-        # bind the test binding to external id = 99
-        self.binder.bind(99, test_binding)
-        # find the odoo binding bound to external record 99
-        binding = self.binder.to_internal(99)
-        self.assertEqual(binding, test_binding)
-        # find the odoo record bound to external record 99
-        record = self.binder.to_internal(99, unwrap=True)
-        self.assertEqual(record, test_record)
-        # find the external record bound to odoo binding
-        external_id = self.binder.to_external(test_binding)
-        self.assertEqual(external_id, 99)
-        # find the external record bound to odoo record
-        external_id = self.binder.to_external(test_record, wrap=True)
-        self.assertEqual(external_id, 99)
-        self.assertEqual(self.binder.unwrap_model(), 'connector.test.record')
-        # unwrapping the binding should give the same binding
-        self.assertEqual(self.binder.unwrap_binding(test_binding), test_record)
+            # bind the test binding to external id = 99
+            self.binder.bind(99, test_binding)
+            # find the odoo binding bound to external record 99
+            binding = self.binder.to_internal(99)
+            self.assertEqual(binding, test_binding)
+            # find the odoo record bound to external record 99
+            record = self.binder.to_internal(99, unwrap=True)
+            self.assertEqual(record, test_record)
+            # find the external record bound to odoo binding
+            external_id = self.binder.to_external(test_binding)
+            self.assertEqual(external_id, 99)
+            # find the external record bound to odoo record
+            external_id = self.binder.to_external(test_record, wrap=True)
+            self.assertEqual(external_id, 99)
+            self.assertEqual(self.binder.unwrap_model(),
+                             'connector.test.record')
+            # unwrapping the binding should give the same binding
+            self.assertEqual(self.binder.unwrap_binding(test_binding),
+                             test_record)

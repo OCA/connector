209ba76058ef3628a5fa3df9103c27fcfa95f0e2
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-22 21:58:10 +0200

    Change Collection.work_on() to a context manager
    It allows to open/close objects on start/end of a synchro / work
    session. The base method does none of that, but this is a really common
    use case. Example: at the beginning of the work session, I open a
    webservice client that is available in our WorkContext for the duration
    of our work. At the end, I close the client to end the connection.

--- component/models/collection.py
+++ component/models/collection.py
@@ -14,6 +14,7 @@ to use in the `_collection` of the Components usable for the Backend.
 
 """
 
+from contextlib import contextmanager
 
 from odoo import models, api
 from ..core import WorkContext
@@ -46,9 +47,9 @@ class Collection(models.AbstractModel):
     Use it::
 
         >>> backend = self.env['magento.backend'].browse(1)
-        >>> work = backend.work_on('magento.sale.order')
-        >>> importer = work.component(usage='magento.sale.importer')
-        >>> importer.run(1)
+        >>> with backend.work_on('magento.sale.order') as work:
+        ...     importer = work.component(usage='magento.sale.importer')
+        ...     importer.run(1)
 
     See also: :class:`odoo.addons.component.core.WorkContext`
 
@@ -57,14 +58,40 @@ class Collection(models.AbstractModel):
     _name = 'collection.base'
     _description = 'Base Abstract Collection'
 
+    @contextmanager
     @api.multi
     def work_on(self, model_name, **kwargs):
-        """ Entry-point for the components
+        """ Entry-point for the components, context manager
 
         Start a work using the components on the model.
         Any keyword argument will be assigned to the work context.
         See documentation of :class:`odoo.addons.component.core.WorkContext`.
 
+        It is a context manager, so you can attach objects and clean them
+        at the end of the work session, such as::
+
+            @contextmanager
+            @api.multi
+            def work_on(self, model_name, **kwargs):
+                self.ensure_one()
+                magento_location = MagentoLocation(
+                    self.location,
+                    self.username,
+                    self.password,
+                )
+                # We create a Magento Client API here, so we can create the
+                # client once (lazily on the first use) and propagate it
+                # through all the sync session, instead of recreating a client
+                # in each backend adapter usage.
+                with MagentoAPI(magento_location) as magento_api:
+                    _super = super(MagentoBackend, self)
+                    # from the components we'll be able to do:
+                    # self.work.magento_api
+                    with _super.work_on(
+                            model_name, magento_api=magento_api, **kwargs
+                            ) as work:
+                        yield work
+
         """
         self.ensure_one()
-        return WorkContext(model_name=model_name, collection=self, **kwargs)
+        yield WorkContext(model_name=model_name, collection=self, **kwargs)

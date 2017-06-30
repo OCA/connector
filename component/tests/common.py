f2a5dff83e7c7c3a5b85c9b851009babcb81db63
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-30 11:02:29 +0200

    Simplify tests by loading modules components
    It the previous commit, @lmignon added the possibility to load all
    components of an addon in a Component Registry.  This commit takes
    benefit of this feature to simplify the existing tests and to add a base
    test case for the Connector (that loads all components of 'component',
    'component_event', 'connector'). It can be used in implementations using
    the connector.

--- component/tests/common.py
+++ component/tests/common.py
@@ -2,46 +2,80 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+import copy
+
 import unittest2
 from odoo.tests import common
 from odoo.addons.component.core import (
-    AbstractComponent,
     ComponentRegistry,
     MetaComponent,
 )
 
 
 class ComponentRegistryCase(unittest2.TestCase):
+    """ This test case can be used as a base for writings tests on components
+
+    It creates a special
+    :class:`odoo.addons.componenent.core.ComponentRegistry` for the purpose
+    of the tests. It loads the ``base`` component in it. In your tests,
+    you can add more components in 2 manners.
+
+    All the components of an Odoo module::
+
+        self._load_module_components('connector')
+
+    Only specific components::
+
+        self._build_components(MyComponent1, MyComponent2)
+
+    Note: for the lookups of the components, the default component
+    registry is a global registry for the database. Here, you will
+    need to explicitly pass ``self.comp_registry`` in the
+    :class:`~odoo.addons.component.core.WorkContext`::
+
+        work = WorkContext(model_name='res.users',
+                           collection='my.collection',
+                           components_registry=self.comp_registry)
+
+    Or::
+
+        collection_record = self.env['my.collection'].browse(1)
+        with collection_record.work_on(
+                'res.partner',
+                components_registry=self.comp_registry) as work:
+
+    """
 
     def setUp(self):
         super(ComponentRegistryCase, self).setUp()
 
         # keep the original classes registered by the metaclass
-        # so we'll restore them at the end of the tests
-        self._original_components = MetaComponent._modules_components.copy()
-        MetaComponent._modules_components.clear()
+        # so we'll restore them at the end of the tests, it avoid
+        # to pollute it with Stub / Test components
+        self._original_components = copy.deepcopy(
+            MetaComponent._modules_components
+        )
 
         # it will be our temporary component registry for our test session
         self.comp_registry = ComponentRegistry()
+
+        # it builds the 'final component' for every component of the
+        # 'component' addon and push them in the component registry
+        self.comp_registry.load_components('component')
+
         # Fake that we are ready to work with the registry
         # normally, it is set to True and the end of the build
         # of the components. Here, we'll add components later in
         # the components registry, but we don't mind for the tests.
         self.comp_registry.ready = True
 
-        # there's always an implicit dependency on a 'base' component
-        # so we must register one
-        class Base(AbstractComponent):
-            _name = 'base'
-
-        # it builds the 'final component' and push it in the component
-        # registry
-        Base._build_component(self.comp_registry)
-
     def tearDown(self):
         super(ComponentRegistryCase, self).tearDown()
         # restore the original metaclass' classes
         MetaComponent._modules_components = self._original_components
+
+    def _load_module_components(self, module):
+        self.comp_registry.load_components(module)
 
     def _build_components(self, *classes):
         for cls in classes:
@@ -50,6 +84,7 @@ class ComponentRegistryCase(unittest2.TestCase):
 
 class TransactionComponentRegistryCase(common.TransactionCase,
                                        ComponentRegistryCase):
+    """ Adds Odoo Transaction in the base Component TestCase """
 
     def setUp(self):
         # resolve an inheritance issue (common.TransactionCase does not use
@@ -57,6 +92,9 @@ class TransactionComponentRegistryCase(common.TransactionCase,
         common.TransactionCase.setUp(self)
         ComponentRegistryCase.setUp(self)
         self.collection = self.env['collection.base']
+
+        # build the components of every installed addons
+        self.env['component.builder'].build_registry(self.comp_registry)
 
     def teardown(self):
         common.TransactionCase.tearDown(self)

cde2ae70d6911667ef316e2e81e687be3c88faec
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-07-04 16:05:59 +0200

    Add new TestCase classes to test connectors
    When we want to test the implementation of a connector, it must be
    simple. Add 2 new test cases for TransactionCase and SavepointCase which
    load all the components of the dependencies addons on the tested addons.
    Only the components of the addons in state 'installed' are loaded, so we
    don't load components of addons that depend on the tested addon.
    
    The tests must call _init_global_registry and build_registry instead of
    _register_hook, so the registry is not set to ready. If the global
    registry is set to ready during tests, the event will trigger during the
    installation of addons which is not what we want.
    
    The existing test case is meant only to be used when one has to
    manipulate components in a custom registry.
    
    See discussion on
    https://github.com/guewen/connector/commit/447b22f8e36c7e258eba5e8b0d93c1577bd50606#commitcomment-22851711

--- test_component/tests/test_components.py
+++ test_component/tests/test_components.py
@@ -1,39 +1,33 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013-2017 Camptocamp SA
+# Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo.tests import common
-from odoo.addons.component.core import _component_databases
+from odoo.addons.component.tests.common import TransactionComponentCase
 
 
-class TestComponentInheritance(common.TransactionCase):
-
-    at_install = False
-    post_install = True
+class TestComponentInheritance(TransactionComponentCase):
 
     def setUp(self):
         super(TestComponentInheritance, self).setUp()
         self.collection = self.env['test.component.collection'].create(
             {'name': 'Test'}
         )
-        dbname = self.env.cr.dbname
-        self.components_registry = _component_databases[dbname]
 
     def test_inherit_base(self):
         with self.collection.work_on('res.users') as work:
-            component = self.components_registry['base'](work)
+            component = work.component_by_name('base')
             self.assertEquals('test_inherit_base',
                               component.test_inherit_base())
 
     def test_inherit_component(self):
         with self.collection.work_on('res.users') as work:
-            component = self.components_registry['mapper'](work)
+            component = work.component_by_name('mapper')
             self.assertEquals('test_inherit_component',
                               component.test_inherit_component())
 
     def test_inherit_prototype_component(self):
         with self.collection.work_on('res.users') as work:
-            component = self.components_registry['test.mapper'](work)
+            component = work.component_by_name('test.mapper')
             self.assertEquals('test_inherit_component',
                               component.test_inherit_component())
             self.assertEquals('test.mapper', component.name())

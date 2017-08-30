f737d67a2f5897b15e64a87b877aecba78df6d2b
Dave Lasley <dave@laslabs.com>
2017-08-30 10:57:56 -0700

    [IMP] component_event: Document implemented events * Document the events that have already been implemented to BaseModel

--- component_event/components/event.py
+++ component_event/components/event.py
@@ -43,6 +43,12 @@ model, so an event can be notified from any model. The
 :meth:`CollectedEvents.notify` method triggers the event and forward the
 arguments to the listeners.
 
+The following events have been implemented into the `'base'` model:
+
+* `on_record_create` - Listener receives args `record, fields`
+* `on_record_write` - Listener receives args `record, fields`
+* `on_record_unlink` - Listener receives args `record`
+
 This should be done only once.
 
 Listeners

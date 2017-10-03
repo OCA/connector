299f5173791c5b02edb33c49d4f10e236d17ae64
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-10-03 14:41:39 +0200

    Remove ConnectorUnit features

--- connector/__init__.py
+++ connector/__init__.py
@@ -3,6 +3,5 @@ from . import components
 from . import setting
 from . import backend_model
 from . import connector
-from . import producer
 from . import checkpoint
 from . import models
--- connector/tests/__init__.py
+++ connector/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import deprecated
 from . import test_advisory_lock
+from . import test_listener
 from . import test_mapper

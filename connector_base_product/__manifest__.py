c25b9b112cde8a829fd296e553dc9048993b79be
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-10-03 10:37:57 +0200

    Make addons installable

--- connector_base_product/__manifest__.py
+++ connector_base_product/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {'name': 'Connector Base Product',
- 'version': '10.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'author': "Openerp Connector Core Editors, Odoo Community Association (OCA)",
  'website': 'http://odoo-connector.com',
  'license': 'AGPL-3',
@@ -21,5 +21,5 @@ Add 'Connector' tab to product view
  'data': [
      'views/product_view.xml'
  ],
- 'installable': False,
+ 'installable': True,
  }

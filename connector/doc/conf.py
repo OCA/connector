647d5572df332763c3fb1af8f9f59720b25482bc
Simone Orsi <simahawk@gmail.com>
2018-03-30 09:22:09 +0200

    connector docs: increase recursion limit

--- connector/doc/conf.py
+++ connector/doc/conf.py
@@ -17,6 +17,9 @@ import sys
 import os
 import sphinx_bootstrap_theme
 
+# Hack to make sphinx happy when there are deeply nested references.
+sys.setrecursionlimit(4000)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

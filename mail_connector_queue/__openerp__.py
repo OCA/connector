815194fd484b5648888c1fe3ed29db2596bcf685
andreparames <github@andreparames.com>
2018-05-23 09:01:04 +0200

    [ADD] New module: mail_connector_queue
    Uses the connector queue to send any emails generated in Odoo.

---
+++ mail_connector_queue/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Mail Connector Queue',
+    'summary': """
+        Email Connector Queue""",
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA',
+    'depends': [
+        'connector',
+    ],
+    'data': [
+        'views/mail_mail.xml',
+    ],
+    'demo': [
+    ],
+}

0585d032207b4b63878f010589dac57d69f8d6ea
andreparames <github@andreparames.com>
2018-05-24 14:17:55 +0200

    Queue job for sending email when state changes to outgoing

--- mail_connector_queue/job.py
+++ mail_connector_queue/job.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp.addons.connector.event import on_record_create
+from openerp.addons.connector.event import on_record_create, on_record_write
 from openerp.addons.connector.queue.job import job
 
 
 @job
 def send_mail(session, mail_id):
+    # make sure two jobs don't send the same email
+    session.cr.execute(
+        "SELECT id FROM mail_mail WHERE id = %s FOR UPDATE", (mail_id,))
+
     mail = session.env['mail.mail'].browse(mail_id)
     if not mail.exists():
         return "mail.mail record (id=%s) no longer exists" % mail_id
@@ -16,11 +20,20 @@ def send_mail(session, mail_id):
         mail.send(auto_commit=False, raise_exception=True)
 
 
-@on_record_create(model_names='mail.mail')
-def mail_creation(session, model_name, record_id, vals):
-    if vals.get('state') != 'outgoing':
-        return
+def queue_job(session, record_id, vals):
     kwargs = {}
     if 'priority' in vals:
         kwargs['priority'] = vals['priority']
     send_mail.delay(session, record_id, **kwargs)
+
+
+@on_record_create(model_names='mail.mail')
+def mail_creation(session, model_name, record_id, vals):
+    if vals.get('state', 'outgoing') == 'outgoing':
+        queue_job(session, record_id, vals)
+
+
+@on_record_write(model_names='mail.mail')
+def mail_write(session, model_name, record_id, vals):
+    if vals.get('state') == 'outgoing':
+        queue_job(session, record_id, vals)

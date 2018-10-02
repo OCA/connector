537fb0bc79afe74105cbe2ad9fd3ee7a90606369
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-10-02 15:24:01 +0200

    Migrate component to version 12.0

--- test_component/__manifest__.py
+++ test_component/__manifest__.py
@@ -4,7 +4,7 @@
 
 {'name': 'Components Tests',
  'summary': 'Automated tests for Components, do not install.',
- 'version': '11.0.1.0.0',
+ 'version': '12.0.1.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Hidden',
@@ -13,5 +13,6 @@
  'website': 'https://www.camptocamp.com',
  'data': ['security/ir.model.access.csv',
           ],
- 'installable': False,
+ 'installable': True,
+ 'maintainers': ['guewen'],
  }

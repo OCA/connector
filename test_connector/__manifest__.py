a1f7161cf289a59d81ec27d117c4b0f69051793c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-11-26 09:14:31 +0100

    Migrate connector to 12.0

--- test_connector/__manifest__.py
+++ test_connector/__manifest__.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {'name': 'Connector Tests',
  'summary': 'Automated tests for Connector, do not install.',
- 'version': '11.0.1.0.0',
+ 'version': '12.0.1.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Hidden',
  'depends': ['connector',
              ],
- 'website': 'https://www.camptocamp.com',
+ 'website': 'http://odoo-connector.com',
  'data': ['security/ir.model.access.csv',
           ],
- 'installable': False,
+ 'installable': True,
  }

086b4b0bcfc72ef3b359d442cc488c557c8df86a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-11-26 09:14:48 +0100

    Migrate component_event to 12.0

--- component_event/__manifest__.py
+++ component_event/__manifest__.py
@@ -3,10 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {'name': 'Components Events',
- 'version': '11.0.1.0.0',
+ 'version': '12.0.1.0.0',
  'author': 'Camptocamp,'
            'Odoo Community Association (OCA)',
- 'website': 'https://www.camptocamp.com',
+ 'website': 'http://odoo-connector.com',
  'license': 'AGPL-3',
  'category': 'Generic Modules',
  'depends': ['component',
@@ -15,5 +15,5 @@
      'python': ['cachetools'],
  },
  'data': [],
- 'installable': False,
+ 'installable': True,
  }

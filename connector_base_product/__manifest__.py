542fc9aed65e3e016133c85eb2e96d2a6a6bd72d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-11-26 09:47:04 +0100

    Migrate connector_base_product to 12.0

--- connector_base_product/__manifest__.py
+++ connector_base_product/__manifest__.py
@@ -1,19 +1,12 @@
-# -*- coding: utf-8 -*-
 # © 2014 David BEAL Akretion, Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {'name': 'Connector Base Product',
- 'version': '11.0.1.0.0',
+ 'version': '12.0.1.0.0',
  'author': "Openerp Connector Core Editors, Odoo Community Association (OCA)",
  'website': 'http://odoo-connector.com',
  'license': 'AGPL-3',
  'category': 'Connector',
- 'description': """
-Connector Base Product
-======================
-
-Add 'Connector' tab to product view
-""",
  'depends': [
      'connector',
      'product',
@@ -21,5 +14,5 @@ Add 'Connector' tab to product view
  'data': [
      'views/product_view.xml'
  ],
- 'installable': False,
+ 'installable': True,
  }

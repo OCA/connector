88be0d93ba6e193ac018de6d898f036d6749613f
mpanarin <m.panarin@mobilunity.com>
2019-02-25 10:12:19 +0200

    [ADD] queue garbage collector methods and cron
    will be moved to the queue repo for version 10-12

--- connector/queue/model.py
+++ connector/queue/model.py
@@ -23,13 +23,15 @@ import os
 import logging
 from datetime import datetime, timedelta
 
-from openerp import models, fields, api, exceptions, _
+from openerp import _, api, exceptions, fields, models
+from openerp.osv import expression
 
-from .job import STATES, DONE, PENDING, OpenERPJobStorage, JOB_REGISTRY
 from .worker import WORKER_TIMEOUT
-from ..session import ConnectorSession
 from .worker import watcher
+
 from ..connector import get_openerp_module, is_module_installed
+from ..session import ConnectorSession
+from .job import DONE, JOB_REGISTRY, PENDING, STATES, OpenERPJobStorage
 
 _logger = logging.getLogger(__name__)
 
@@ -212,6 +214,55 @@ class QueueJob(models.Model):
         )
         jobs.unlink()
         return True
+
+    @api.model
+    def requeue_stuck_jobs(self, enqueued_delta=5, started_delta=0):
+        """Fix jobs that are in a bad states
+        :param in_queue_delta: lookup time in minutes for jobs
+                                that are in enqueued state
+
+        :param started_delta: lookup time in minutes for jobs
+                                that are in enqueued state,
+                                0 means that it is not checked
+        """
+        self._get_stuck_jobs_to_requeue(
+            enqueued_delta=enqueued_delta,
+            started_delta=started_delta
+        ).requeue()
+        return True
+
+    @api.model
+    def _get_stuck_jobs_domain(self, queue_dl, started_dl):
+        domain = []
+        now = fields.datetime.now()
+        if queue_dl:
+            queue_dl = now - timedelta(minutes=queue_dl)
+            domain.append([
+                '&',
+                ('date_enqueued', '<=', fields.Datetime.to_string(queue_dl)),
+                ('state', '=', 'enqueued'),
+            ])
+        if started_dl:
+            started_dl = now - timedelta(minutes=started_dl)
+            domain.append([
+                '&',
+                ('date_started', '<=', fields.Datetime.to_string(started_dl)),
+                ('state', '=', 'started'),
+            ])
+        if not domain:
+            raise exceptions.ValidationError(
+                _("If both parameters are 0, ALL jobs will be requeued!")
+            )
+        return expression.OR(domain)
+
+    @api.model
+    def _get_stuck_jobs_to_requeue(self, enqueued_delta, started_delta):
+        job_model = self.env['queue.job']
+        stuck_jobs = job_model.search(self._get_stuck_jobs_domain(
+            enqueued_delta,
+            started_delta,
+        ))
+        return stuck_jobs
 
 
 class QueueWorker(models.Model):

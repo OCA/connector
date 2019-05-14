46c386f1b65c5c5ef392c108d694cbdc61cd28c2
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-05-14 15:26:00 +0200

    [FIX] setup.py

--- setup/component/setup.py
+++ setup/component/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addons={
+    odoo_addon={
         'external_dependencies_override': {
             'python': {
                 'cachetools': 'cachetools>=2.0.1,<3.0.0',

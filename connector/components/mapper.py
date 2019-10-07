0c5e5a3f9ca3343e64ab747b6471f5c43833e0d2
andreinl <andrei.levin@didotech.com>
2019-10-07 14:16:30 +0200

    Improved Submapping documentation

--- connector/components/mapper.py
+++ connector/components/mapper.py
@@ -530,6 +530,48 @@ class Mapper(AbstractComponent):
         >>> values = map_record.values(only_create=True)
         >>> values = map_record.values(fields=['name', 'street'])
 
+    Example of making 'children' records as 'create_only'::
+
+        class FooSaleOrderMapper(Component):
+            _name = 'foo.sale.order.import.mapper'
+            _inherit = 'foo.import.mapper'
+            _apply_on = 'foo.sale.order'
+
+            children = [('items', 'line_ids', 'foo.sale.order.line')]
+
+
+        class SaleLineMapChild(Component):
+            _name = 'foo.sale.line.import.map.child'
+            _inherit = ['foo.connector', 'base.map.child.import']
+            _apply_on = 'foo.sale.order.line'
+
+            def skip_item(self, map_record):
+                record = map_record.source
+                if not record['attributes']['quantity']:
+                    return True
+
+            def get_item_values(self, map_record, to_attr, options):
+                values = map_record.values(**options)
+                binder = self.binder_for()
+                binding = binder.to_internal(map_record.source['id'])
+                if binding:
+                    # already exists, keeps the id
+                    values['id'] = binding.id
+                return values
+
+            def format_items(self, items_values):
+                # if we already have an ID (found in get_item_values())
+                # we change the command to update the existing record
+                items = []
+                for item in items_values[:]:
+                    if item.get('id'):
+                        binding_id = item.pop('id')
+                        # update the record
+                        items.append((1, binding_id, item))
+                    else:
+                        # create the record
+                        items.append((0, 0, item))
+                return items
     """
 
     _name = 'base.mapper'

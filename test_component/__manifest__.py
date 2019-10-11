15f1c28a29f2d48fcb67d02e480a5704c0adbf39
Damien Crier <damien.crier@camptocamp.com>
2019-10-11 10:15:58 +0200

    [MIG] test_component: Migration to 13.0

--- test_component/__manifest__.py
+++ test_component/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA
+# Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {'name': 'Components Tests',
  'summary': 'Automated tests for Components, do not install.',
- 'version': '12.0.1.0.0',
+ 'version': '13.0.1.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Hidden',
@@ -13,7 +13,7 @@
  'website': 'https://www.camptocamp.com',
  'data': ['security/ir.model.access.csv',
           ],
- 'installable': False,
+ 'installable': True,
  'development_status': 'Stable',
  'maintainers': ['guewen'],
  }

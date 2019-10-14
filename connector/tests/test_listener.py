29f87968ff51f0d84c78c4cef32f5da7c8d771d7
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-14 11:33:03 +0200

    [12.0][CHG] connector: Use more permissive licence: AGPL-> LGPL

--- connector/tests/test_listener.py
+++ connector/tests/test_listener.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 import mock
 

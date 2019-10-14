ad5aaca16c2df93c3571f36879028df8b0c326c1
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-14 11:33:03 +0200

    [12.0][CHG] base_rest: Use more permissive licence: AGPL-> LGPL

--- connector_base_product/__manifest__.py
+++ connector_base_product/__manifest__.py
@@ -1,11 +1,11 @@
 # © 2014 David BEAL Akretion, Sodexis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {'name': 'Connector Base Product',
  'version': '12.0.1.0.0',
  'author': "Openerp Connector Core Editors, Odoo Community Association (OCA)",
  'website': 'http://odoo-connector.com',
- 'license': 'AGPL-3',
+ 'license': 'LGPL-3',
  'category': 'Connector',
  'depends': [
      'connector',

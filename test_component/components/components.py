7202d61b31526024a1dcbc05fbf840dac30ae999
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-23 08:25:12 +0200

    black, isort

--- test_component/components/components.py
+++ test_component/components/components.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
@@ -6,31 +5,31 @@ from odoo.addons.component.core import AbstractComponent, Component
 
 
 class BaseComponent(AbstractComponent):
-    _inherit = 'base'
+    _inherit = "base"
 
     def test_inherit_base(self):
-        return 'test_inherit_base'
+        return "test_inherit_base"
 
 
 class Mapper(AbstractComponent):
-    _name = 'mapper'
+    _name = "mapper"
 
     def test_inherit_component(self):
-        return 'test_inherit_component'
+        return "test_inherit_component"
 
 
 class ImportTestMapper(Component):
-    _name = 'test.mapper'
-    _inherit = 'mapper'
-    _usage = 'import.mapper'
-    _collection = 'test.component.collection'
+    _name = "test.mapper"
+    _inherit = "mapper"
+    _usage = "import.mapper"
+    _collection = "test.component.collection"
 
     def name(self):
-        return 'test.mapper'
+        return "test.mapper"
 
 
 class UserTestComponent(Component):
-    _name = 'test.user.component'
-    _apply_on = ['res.users']
-    _usage = 'test1'
-    _collection = 'test.component.collection'
+    _name = "test.user.component"
+    _apply_on = ["res.users"]
+    _usage = "test1"
+    _collection = "test.component.collection"
--- test_component/tests/__init__.py
+++ test_component/tests/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 from . import test_components
 from . import test_component_collection

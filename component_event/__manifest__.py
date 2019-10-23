308f4f0f86f8f868f52e26eaf51e6c95aa7cd5d4
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-23 08:25:12 +0200

    black, isort

--- component_event/__manifest__.py
+++ component_event/__manifest__.py
@@ -1,18 +1,15 @@
 # Copyright 2019 Camptocamp SA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-{'name': 'Components Events',
- 'version': '13.0.1.0.0',
- 'author': 'Camptocamp,'
-           'Odoo Community Association (OCA)',
- 'website': 'http://odoo-connector.com',
- 'license': 'LGPL-3',
- 'category': 'Generic Modules',
- 'depends': ['component',
-             ],
- 'external_dependencies': {
-     'python': ['cachetools'],
- },
- 'data': [],
- 'installable': False,
- }
+{
+    "name": "Components Events",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp," "Odoo Community Association (OCA)",
+    "website": "http://odoo-connector.com",
+    "license": "LGPL-3",
+    "category": "Generic Modules",
+    "depends": ["component"],
+    "external_dependencies": {"python": ["cachetools"]},
+    "data": [],
+    "installable": False,
+}

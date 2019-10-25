8b1bc8979b900ca8e84892254c84f6c331b9e4e3
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-25 15:20:35 +0200

    connector, test_connector: black isort

--- test_connector/models/components.py
+++ test_connector/models/components.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Camptocamp SA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
@@ -6,22 +5,18 @@ from odoo.addons.component.core import Component
 
 
 class ConnectorTestBinder(Component):
-    _name = 'connector.test.binder'
-    _inherit = ['base.binder']
-    _apply_on = [
-        'connector.test.binding',
-    ]
+    _name = "connector.test.binder"
+    _inherit = ["base.binder"]
+    _apply_on = ["connector.test.binding"]
 
 
 class NoInheritsBinder(Component):
-    _name = 'connector.test.no.inherits.binder'
-    _inherit = ['base.binder']
-    _apply_on = [
-        'no.inherits.binding',
-    ]
+    _name = "connector.test.no.inherits.binder"
+    _inherit = ["base.binder"]
+    _apply_on = ["no.inherits.binding"]
 
     def unwrap_binding(self, binding):
-        raise ValueError('Not an inherits')
+        raise ValueError("Not an inherits")
 
     def unwrap_model(self):
-        raise ValueError('Not an inherits')
+        raise ValueError("Not an inherits")

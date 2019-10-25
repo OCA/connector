9019815de5374785bae3cd2f5abf17474d8733a8
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-25 15:20:35 +0200

    connector, test_connector: black isort

--- test_connector/__manifest__.py
+++ test_connector/__manifest__.py
@@ -1,16 +1,15 @@
 # Copyright 2017 Camptocamp SA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-{'name': 'Connector Tests',
- 'summary': 'Automated tests for Connector, do not install.',
- 'version': '13.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA)',
- 'license': 'LGPL-3',
- 'category': 'Hidden',
- 'depends': ['connector',
-             ],
- 'website': 'http://odoo-connector.com',
- 'data': ['security/ir.model.access.csv',
-          ],
- 'installable': True,
- }
+{
+    "name": "Connector Tests",
+    "summary": "Automated tests for Connector, do not install.",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "category": "Hidden",
+    "depends": ["connector"],
+    "website": "http://odoo-connector.com",
+    "data": ["security/ir.model.access.csv"],
+    "installable": True,
+}

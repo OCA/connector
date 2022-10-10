ecf317f6e927561b4f6430d5c434183d03b6d0b7
Ruchir Shukla <ruchir@bizzappdev.com>
2022-10-10 15:31:38 +0530

    [IMP] test_component:  black, isort, prettier

--- test_component/__manifest__.py
+++ test_component/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Components Tests",
     "summary": "Automated tests for Components, do not install.",
-    "version": "15.0.1.0.0",
+    "version": "16.0.0.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "category": "Hidden",

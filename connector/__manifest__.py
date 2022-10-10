045deeefa062d103e1360609cb41fa653d385c0d
Ruchir Shukla <ruchir@bizzappdev.com>
2022-10-10 17:50:58 +0530

    [IMP] connector: pre-commit stuff

--- connector/__manifest__.py
+++ connector/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Connector",
-    "version": "15.0.1.0.4",
+    "version": "16.0.0.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/connector",
     "license": "LGPL-3",

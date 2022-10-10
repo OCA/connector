f261057a8423eb1c0bfc7253c153e612eb7a3459
Ruchir Shukla <ruchir@bizzappdev.com>
2022-10-10 15:11:06 +0530

    [IMP] component: black, isort, prettier

--- component/__manifest__.py
+++ component/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Components",
     "summary": "Add capabilities to register and use decoupled components,"
     " as an alternative to model classes",
-    "version": "15.0.1.0.1",
+    "version": "16.0.0.0.0",
     "author": "Camptocamp," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/connector",
     "license": "LGPL-3",

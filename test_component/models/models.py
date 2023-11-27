97b03d74a1e5437fa4deac5b9042ca549e197ec8
chien <chien@trobz.com>
2023-11-27 10:43:54 +0700

    [IMP] test_component: black, isort, prettier

--- test_component/models/models.py
+++ test_component/models/models.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 
 
 class TestComponentCollection(models.Model):
-
     _name = "test.component.collection"
     _description = "Test Component Collection"
     _inherit = ["collection.base"]

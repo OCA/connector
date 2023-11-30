899612440e87a1087655250bec1069be63b139d1
chien <chien@trobz.com>
2023-11-30 09:53:29 +0700

    [MIG] component: Migration to 17.0

--- component/__manifest__.py
+++ component/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Components",
     "summary": "Add capabilities to register and use decoupled components,"
     " as an alternative to model classes",
-    "version": "16.0.1.1.1",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/connector",
     "license": "LGPL-3",

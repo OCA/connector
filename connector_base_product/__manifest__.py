84937c3bc05875a08eb1237c2212216060fcf532
Maksym Yankin <yankinmk@gmail.com>
2025-01-21 13:42:00 +0200

    [18.0][MIG] connector_base_product: Migration to 18.0

--- connector_base_product/__manifest__.py
+++ connector_base_product/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Connector Base Product",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Openerp Connector Core Editors, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/connector",
     "license": "LGPL-3",

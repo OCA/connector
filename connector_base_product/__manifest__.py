22b50da3f1d521a0afdef031ef62bc26bcf023da
Sergij Pfaifer <sergiipf@gmail.com>
2026-03-10 09:30:47 +0200

    [MIG] connector_base_product: Migration to 19.0

--- connector_base_product/__manifest__.py
+++ connector_base_product/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Connector Base Product",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Openerp Connector Core Editors, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/connector",
     "license": "LGPL-3",
